f3ce3c28b3479946de452e90cf17ce1f19db7fb7
stephenhu <hus@vmware.com>
2013-01-19 14:16:23 +0800

    draft schema

--- db/migrate/001_init_schema.rb
+++ db/migrate/001_init_schema.rb
@@ -3,12 +3,60 @@ class InitSchema < ActiveRecord::Migration
   def self.up
 
     create_table :users do |t|
+      t.string :uuid, :unique => true
+      t.string :salt
+      t.string :token
+      t.timestamps 
+    end
 
+    create_table :followers do |t|
+      t.belongs_to :user
+      t.integer :user_id
+      t.integer :follow_id
+      t.timestamps
+    end
+
+    create_table :hashtags do |t|
+      t.string :hashtag, :null => false, :unique => true
+      t.timestamps
+    end
+
+    create_table :actions do |t|
+      t.integer :user_id
+      t.integer :delegate_id
+      t.string :action, :null => false
+      t.boolean :visible, :default => false
+      t.integer :estimate
+      t.timestamp :finished
+      t.integer :priority
+      t.integer :parent_id
+      t.integer :ordinal
+      t.timestamps
+    end
+
+    create_table :actionhashtags do |t|
+      t.integer :action_id
+      t.integer :hashtag_id
+      t.timestamps
+    end
+
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :action_id
+      t.string :comment
+      t.timestamps
     end
 
   end
 
   def self.down
+
+    drop_table :users
+    drop_table :followers
+    drop_table :hashtags
+    drop_table :actions
+    drop_table :actionhashtags
+    drop_table :comments
 
   end
 
