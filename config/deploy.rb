1e5ab1d948525b27060b32dcece45b6ed5eba4bc
stephenhu <hu@chef.(none)>
2013-07-12 17:27:48 +0800

    fix sudo users

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,25 +2,33 @@ set :application, "getsdone"
 set :scm, :git
 set :repository,  "git@github.com:stephenhu/getsdone"
 set :deploy_to, "/web/#{application}"
-set :user_sudo, false
+set :use_sudo, false
 
 set :user, "devops"
 set :group, user
 set :runner, user
 
-set :host "#{user}@192.168.174.135"
+set :host, "#{user}@192.168.174.135"
 role :web, host
 role :app, host
+role :db, host
 
 set :rails_env, :production
 
+set_default( :postgresql_password, "steve" )
+namespace :postgresql do
+  desc "create database"
+  task :create, roles: :db do
+    run "psql -U postgres -W -c 'create database getsdone'"
+  end
+end
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
