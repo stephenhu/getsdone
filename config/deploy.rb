c0b363a9030acf80d619d114442f5e4747193f39
hu <hus@vmware.com>
2013-07-24 02:12:07 +0800

    rbenv configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,8 @@ role :web, host
 role :app, host
 role :db,  host
 
+HOME = "/home/devops"
+
 set :rails_env, :production
 
 namespace :postgresql do
@@ -47,47 +49,44 @@ namespace :github do
   desc "configure github environment"
   task :setup do
     email     = Capistrano::CLI.ui.ask("input email address: ")
-    file      = Capistrano::CLI.ui.ask("input public key path: ")
-    username  = Capistrano::CLI.ui.ask("input github username: ")
-    password  = Capistrano::CLI.password_prompt("input github password: ")
+    file      = "/home/devops/.ssh/id_rsa"
+    public_file = "#{file}.pub"
     run "git config --global core.editor 'vi'"
     run "git config --global user.email '#{email}'"
-    run "ssh-keygen -t rsa -C '#{email}' -N '' -f '#{file}'"
-    public_file = "#{file}.pub"
+    check = capture "if [ -f #{file} ]; then echo 'true'; fi"
+    if check.empty?
+      run "ssh-keygen -q -t rsa -C '#{email}' -N '' -f '#{file}'"
+    end
     key = capture "cat #{public_file}"
+    username  = Capistrano::CLI.ui.ask("input github username: ")
+    password  = Capistrano::CLI.password_prompt("input github password: ")
     github = Github.new( login: username, password: password )
     github.users.keys.create( title: "capistrano generated", key: key )
+
   end
 
 end
 
-namespace :ruby do
+namespace :rbenv do
 
   desc "install rbenv"
-  task :git do
+  task :setup do
+    check = capture "if [ -d #{HOME}/.rbenv ]; then echo 'true'; fi"
+    if check.empty?
+      run "git clone git@github.com:sstephenson/rbenv ~/.rbenv"
+    end
+    check2 = capture "grep rbenv #{HOME}/.bash_profile"
+    puts "check2: #{check2}"
+    if not check2.include?("rbenv")
+      run "echo 'export PATH=\"#{HOME}/.rbenv/bin:$PATH\"' >> #{HOME}/.bash_profile"
+      run "echo 'eval \"$(rbenv init -)\"' >> #{HOME}/.bash_profile"
+    end
+    check3 = capture "if [ -d #{HOME}/.rbenv/plugins/ruby-build ]; then echo 'true'; fi"
+    if check3.empty?
+      run "git clone git@github.com:sstephenson/ruby-build ~/.rbenv/plugins/ruby-build"
+      run "exec $SHELL -l"
+      run "rbenv rehash"
+    end
   end
-
 end
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
